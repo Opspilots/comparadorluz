2d10143110a5428749a2a4f7bcce90eb01d976ac
juanortiz116 <juanortiz11615@gmail.com>
2026-02-09 21:50:40 +0100

    32

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,6 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
-    define: {
-        global: 'window',
-    },
     test: {
         globals: true,
         environment: 'jsdom',
